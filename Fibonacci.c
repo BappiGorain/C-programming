102dc2ece6dca0bccbe01d642f3701cbfbb898b0
Bappi Gorain <146017226+BappiGorain@users.noreply.github.com>
2024-09-19 15:18:43 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+void print_array(int *arr, int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",*(arr+i));
+    }
+}
+
+void fibo(int *f, int *s, int n)
+{
+    int arr[n];
+    arr[0] = *f;
+    arr[1] = *s;
+    int index = 2;
+    int sum = 0;
+    
+    while(index < n)
+    {
+        sum = *f + *s;
+
+        arr[index] = sum;
+        index++;
+        
+        int temp = *s;
+        *s = sum;
+        *f = temp;
+    }
+
+    print_array(arr,n);
+
+}
+
+int main()
+{
+    int f = 0;
+    int s = 1;
+
+    printf("Enter the value of n: ");
+    int n;
+    scanf("%d",&n);
+    printf("\n");
+
+    fibo(&f,&s,n);
+
+}
